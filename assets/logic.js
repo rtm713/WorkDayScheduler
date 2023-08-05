1a2b1de6a110c255caa724e47104726621b5f86e
rtm713 <rtm713@gmail.com>
2023-08-05 18:48:30 -0500

    cleaned up code and removed comments

--- assets/logic.js
+++ assets/logic.js
@@ -31,9 +31,4 @@ $(document).ready(function () {
       currentBlock.classList.add('present');
     }
   });
-  });
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+});
